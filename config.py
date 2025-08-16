5f257babaaf3c876f21a8e50a1d3602d3a81f07e
Pantat Suwanwatthana <97791735+KenROCKLEE@users.noreply.github.com>
2025-08-16 11:11:30 +0700

    config

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import torch
 import torchvision
 
-def check_setup():
+def check_gpu():
     print("PyTorch version:", torch.__version__)
     print("Torchvision version:", torchvision.__version__)
-
-    # Check if GPU is available
     if torch.cuda.is_available():
-        print("CUDA is available! Using GPU:", torch.cuda.get_device_name(0))
+        print("CUDA version:", torch.version.cuda)
+        print("GPU is available! Using GPU.")
     else:
         print("CUDA not available. Using CPU.")
 
 if __name__ == "__main__":
-    check_setup()
+    check_gpu()
